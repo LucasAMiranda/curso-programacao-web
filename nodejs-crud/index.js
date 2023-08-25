9b0dc19ea41eac8a06eaea44d817fefe47291656
lucas A.miranda <lucasa.miranda@hotmail.com>
2023-08-25 16:15:20 -0300

    Subindo o Projeto de Crud do NodeJs Atualizado

--- nodejs-crud/index.js
+++ nodejs-crud/index.js
@@ -1,63 +1,84 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const axios = require('axios');
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password : '',
-    database : 'crud_app'
+    password: '',
+    database: 'crud_app'
 });
 
 db.connect((err) => {
     if (err) throw err;
-    console.log('Connected to Mysql database');
+    console.log('Connected to MySQL database');
 });
 
 app.get('/', (req, res) => {
-    const sql = 'SELECT * FROM tasks';
+    const sql = 'SELECT * FROM addresses';
     db.query(sql, (err, results) => {
-        if(err) throw err;
-        res.render('index', {tasks: results});
+        if (err) throw err;
+        res.render('index', { addresses: results });
     });
+});
+
+app.post('/add', async (req, res) => {
+    const { cep } = req.body;
+    try {
+        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+        const addressData = response.data;
+        
+        const { logradouro, bairro, localidade, uf } = addressData;
+
+        const sql = 'INSERT INTO addresses (logradouro, bairro, localidade, uf) VALUES (?, ?, ?, ?)';
+        db.query(sql, [logradouro, bairro, localidade, uf], (err, result) => {
+            if (err) throw err;
+            console.log('Address added');
+            res.redirect('/');
+        });
+    } catch (error) {
+        console.error(error);
+        res.redirect('/');
+    }
 });
 
 app.get('/edit/:id', (req, res) => {
-    const taskId = req.params.id;
-    const sql = 'SELECT * FROM tasks WHERE id = ?';
-    db.query(sql, [taskId], (err, result) =>{
-        if (err) throw err;
-        res.render('edit', {task:result[0]});
+    const addressId = req.params.id;
+    const sql = 'SELECT * FROM address WHERE id = ?';
+    db.query(sql, [addressId], (err, resul) => {
+        if(err) throw err;
+        res.render('edit', {address: result[0]});
     });
 });
 
-app.post('/update/:id', (req, res) => {
-    const taskId = req.params.id;
-    const {title, description} = req.body;
-    const sql = 'UPDATE tasks SET title= ?, description=? WHERE id= ?';
-    db.query(sql, [title, description, taskId], (err, result)=>{
-        if(err) throw err;
-        console.log('Task updated');
-        res.redirect('/');
-    });
-})
-
-app.post('/delete/:id', (req, res)=>{
-    const taskId = req.params.id;
-    const sql = 'DELETE FROM tasks WHERE id = ?';
-    db.query(sql, [taskId], (err, result) => {
+app.put('update/:id', (req, res) => {
+    const addressId = req.params.id;
+    const sql = 'UPDATE addresses SET logradouro = ?, bairro= ?, localidade=?, uf=? WHERE id=?';
+    db.query(sql, [addressId], (err, result) => {
         if (err) throw err;
-        console.log('Task deleted');
+        console.log('Address deleted');
+     res.redirect('/');
+    });
+
+});
+
+app.post('/delete/:id', (req, res) => {
+    const addressId = req.params.id;
+    const sql = 'DELETE FROM addresses WHERE id = ?';
+    db.query(sql, [addressId], (err, result) => {
+        if (err) throw err;
+        console.log('Address deleted');
         res.redirect('/');
     });
 });
 
 app.listen(port, () => {
-    console.log(`Servidor executando na porta ${port}`)
+    console.log(`Servidor executando na porta ${port}`);
 });
