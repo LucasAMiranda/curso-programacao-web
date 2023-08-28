89aee76901d983c243c278a03bd3073216918f53
lucas A.miranda <lucasa.miranda@hotmail.com>
2023-08-28 14:49:48 -0300

    Projeto finalizado com sucesso Endereco API com ReactJs

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,54 +1,53 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import AddressForm from "./components/AddressForm";
 import AddressDetail from "./components/AddressDetail";
-import {Container, Typography, Paper} from '@mui/material';
-import {styled} from '@mui/material/styles';
+import { Container, Typography, Paper } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import './App.css';
+import MultiTheme from "./components/MultiTheme";
+import { ThemeProvider } from "@mui/material/styles";
 
-const StyledContainer = styled(Container)(({ theme }) =>({
-    textAlign: 'center',
-    padding: theme.spacing(3)
+const StyledContainer = styled(Container)(({ theme }) => ({
+  textAlign: 'center',
+  padding: theme.spacing(3)
 }));
 
-const StyledPaper = styled(Paper)(( {theme} ) => ({
-    padding: theme.spacing(2),
-    marginBottom: theme.spacing(3),
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+  marginBottom: theme.spacing(3),
 }));
-
 
 const App = () => {
 
   const [address, setAddress] = useState(null);
 
-  const searchAddress = async (cep) =>{
-    try{
+  const searchAddress = async (cep) => {
+    try {
       const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
       const data = await response.json();
-      setAddress(data)
-    }catch(error){
-      console.error('Erro ao buscar endereço: ', error)
+      setAddress(data);
+    } catch (error) {
+      console.error('Erro ao buscar endereço: ', error);
     }
   }
-  
+
   return (
-    <StyledContainer>
-       <StyledPaper elevation={3}>
-           <Typography variant="h4" gutterBottom>
-              Busca de Endereço
-           </Typography>
-        
-              <AddressForm onSearch={searchAddress} />
-         
-       </StyledPaper>
-       {address && (
+    <ThemeProvider theme={MultiTheme}>
+      <StyledContainer>
+        <StyledPaper elevation={3}>
+          <Typography variant="h4" gutterBottom>
+            Busca de Endereço
+          </Typography>
+          <AddressForm onSearch={searchAddress} />
+        </StyledPaper>
+        {address && (
           <StyledPaper elevation={3}>
             <AddressDetail address={address} />
           </StyledPaper>
-       )}
-    </StyledContainer>
-   
+        )}
+      </StyledContainer>
+    </ThemeProvider>
   );
-
 }
 
 export default App;
---
+++ meu-projeto/src/components/MultiTheme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+const MultiTheme = createTheme({
+
+    palette:{
+        primary:{
+            main:'#1976d2'
+        },
+
+        secondary:{
+            main:'#f50057'
+        },
+    },
+})
+
+export default MultiTheme;
